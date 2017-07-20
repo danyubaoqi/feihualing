3d87af6474d2e265870ad26a061132585d5e7301
danyubaoqi <1600093003@pku.edu.cn>
2017-07-20 11:46:22 +0800

    heiheihei
    heiheihei

--- FHL.py
+++ FHL.py
@@ -1,5 +1,5 @@
 from wxpy import *
-from random import shuffle
+from random import shuffle,randint
 
 kaishi = 0
 bot = Bot(cache_path=True)
@@ -7,10 +7,15 @@ fhlG = bot.groups().search("飞花令")[0]
 number = 1
 zanshi1 = []
 zi = ""
-opFile = open("data3.txt", "r", encoding="utf-8")
+opFile = open("shicidata.txt", "r", encoding="utf-8")
 data = opFile.readlines()
 opFile.close()
-
+leixing = 0
+lenD=len(data)
+dict={}
+for i in range(10):
+    dict[i]=data[0+i*600000:600000+i*600000]
+dict[10]=data[6000000:lenD]
 
 def isShi(juzi):
     for i in data:
@@ -20,31 +25,41 @@ def isShi(juzi):
 
 
 def chushihua():
-    global number, zanshi1, zi, kaishi
+    global number, zanshi1, zi, kaishi,leixing
     number = 1
     zanshi1 = []
     zi = ""
     kaishi = 0
+    leixing = 0
 
 
 def help_FHL(msg):
-    global number, zi, kaishi, data
-    data = shuffle(data)
+    global number, zi, kaishi, dict
+    ranNum=randint(0,10)
     if kaishi == 1:
-        number -= 1
-        for i in data:
-            if len(i) > 2 and number < len(i) and i[number] == zi:
-                if (i not in zanshi1):
-                    zanshi1.append(i)
-                    msg.reply(str(i))
-                number += 2
-                break
+        if leixing==1:
+            for i in dict[ranNum]:
+                if zi in i:
+                    if (i not in zanshi1):
+                        zanshi1.append(i)
+                        msg.reply(str(i))
+                    break
+        else:
+            number-=1
+            for i in dict[ranNum]:
+                if len(i) > 2 and number < len(i) and i[number] == zi:
+                    if (i not in zanshi1):
+                        zanshi1.append(i)
+                        msg.reply(str(i))
+                    number += 2
+                    break
+
     else:
         msg.reply("伦家让你先说嘛")
 
 
 def start_FHL(juzi, msg):
-    global number, kaishi, zi
+    global number, kaishi, zi,dict
     if number == 1:
         zi = juzi[0]
     elif zi != juzi[number - 1]:
@@ -53,31 +68,72 @@ def start_FHL(juzi, msg):
     if (number == 7):
         chushihua()
         msg.reply("飞花令已结束")
+    zanshi1.append(juzi)
+    ranNum = randint(0, 10)
+    if isShi(juzi):
+        for i in dict[ranNum]:
+            if len(i) > 2 and number < len(i) and i[number] == zi:
+                if (i not in zanshi1):
+                    zanshi1.append(i)
+                    msg.reply(str(i))
+                number += 2
+                break
+    else:
+        msg.reply("这不是诗")
+    shuffle(dict[ranNum])
 
-    for i in data:
-        if len(i) > 2 and number < len(i) and i[number] == zi:
-            if (i not in zanshi1):
-                zanshi1.append(i)
-                msg.reply(str(i))
-                zanshi1.append(juzi)
-            number += 2
-            break
+
+def start_FHL_easy(juzi, msg):
+
+    global number, kaishi, zi, dict
+    if number == 1:
+        zi = juzi[0]
+        number = 0
+    if zi not in juzi:
+        msg.reply("嘿嘿嘿")
+        return 0
+    if juzi in zanshi1:
+        msg.reply("你说过了")
+    else:
+        zanshi1.append(juzi)
+    ranNum=randint(0,10)
+    if isShi(juzi):
+        for i in dict[ranNum]:
+            if zi in i:
+                if (i not in zanshi1):
+                    zanshi1.append(i)
+                    msg.reply(str(i))
+                    break
+    else:
+        msg.reply("这不是诗")
+    shuffle(dict[ranNum])
 
 
 def function_find(txt, msg):
-    global kaishi, number, zanshi1
+    global leixing, kaishi, number, zanshi1
     print(msg)
     if txt == "开始飞花令":
-        msg.reply("飞花令已开始,请输入第一句")
+        chushihua()
+        msg.reply("飞花令已开始,请输入类型 无限 或者 有限")
         kaishi = 1
     elif txt == "终止":
         chushihua()
         msg.reply("飞花令已终止")
-    elif txt == "帮助":
+    elif txt == "无限":
+        msg.reply("请输入第一句诗")
+        leixing = 1
+    elif txt == "有限":
+        msg.reply("请输入第一句诗")
+        leixing = 2
+    elif txt=="帮助":
         help_FHL(msg)
     elif kaishi == 1:
-        juzi = txt
-        start_FHL(juzi, msg)
+        if leixing == 1:
+            juzi = txt
+            start_FHL_easy(juzi, msg)
+        else:
+            juzi = txt
+            start_FHL(juzi, msg)
 
 
 @bot.register()
